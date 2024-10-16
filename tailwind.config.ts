3b7fc976fb70d257e7235ea4994df699a9d2ab90
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-10-16 23:58:34 +0700

    feat: add tailwindconfiguration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -11,9 +14,26 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: "#0288D1",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors
+  ],
+  darkMode: "class",
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
