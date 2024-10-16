edc472c560fad32cddb7ba7a348da71c075429e5
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-10-16 23:58:41 +0700

    feat: add types

---
+++ src/app/types.ts
@@ -0,0 +1,9 @@
+// types.ts
+export interface Device {
+    deviceid: string;
+    longitude: string;
+    status: string;
+    location: string;
+    latitude: string;
+    avg_device_count: number;
+  }
