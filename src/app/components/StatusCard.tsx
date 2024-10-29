27c4ea3a608f1189d6f91aeaf68896317acaef0b
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-10-29 13:46:00 +0700

    style: re-styling status card

--- src/app/components/StatusCard.tsx
+++ src/app/components/StatusCard.tsx
@@ -7,8 +7,16 @@ interface StatusCardProps {
 }
 
 const StatusCard: React.FC<StatusCardProps> = ({ color, title, description }) => {
+  const bgColorClass = {
+    "blue": "bg-[#0288D1]",
+    "orange": "bg-orange-500",
+    "green": "bg-green-500",
+    "red": "bg-[#D32F2F]"
+    // Tambahkan mapping warna lainnya sesuai kebutuhan
+  }[color] || 'bg-gray-500'; // Default jika warna tidak sesuai
+
   return (
-    <div className={`bg-[${color}] p-4 rounded-lg text-white`}>
+    <div className={`${bgColorClass} p-4 rounded-lg text-white shadow-2xl`}>
       <h3 className="font-bold">{title}</h3>
       <p>{description}</p>
     </div>
