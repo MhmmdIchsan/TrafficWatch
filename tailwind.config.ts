c266008d7a05e8a212fe08091afd03b2db02f135
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-10-30 01:04:10 +0700

    refactor: update dark mode class declaration in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
     addVariablesForColors,
       require("tailwindcss-animate")
 ],
-  darkMode: ["class", "class"],
+	darkMode: 'class',
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
