740c81e630500366bb5a4a2c946911b518625ebb
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-11-28 20:12:08 +0700

    feat: add some data types

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,9 +1,19 @@
 // types.ts
 export interface Device {
+    last_update_time: string;
     deviceid: string;
     longitude: string;
     status: string;
     location: string;
     latitude: string;
     avg_device_count: number;
+    is_detection_enabled: boolean; // Menambahkan tipe is_detection_enabled
+    sampling_interval: number; // Menambahkan tipe sampling_interval
+  }
+
+  export interface Notification {
+    id: string;
+    type: 'warning' | 'alert' | 'info';
+    message: string;
+    timestamp: string;
   }
